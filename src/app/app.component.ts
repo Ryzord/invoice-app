298d850a8984b154c2039af606888f14f4cbcec1
Ryzord <herrera_ruben1@hotmail.com>
2025-05-07 00:41:38 +0200

    Implemented bootstrap on invoice component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InvoiceComponent } from './components/invoice/invoice.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [InvoiceComponent, CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
