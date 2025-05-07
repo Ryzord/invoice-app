f9b6c9a578b3593d57741884f3e7094ac295e142
Ryzord <herrera_ruben1@hotmail.com>
2025-05-07 10:11:56 +0200

    Added Total component.

--- src/app/services/invoice.service.ts
+++ src/app/services/invoice.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Invoice } from '../models/invoice';
 import { invoiceData } from '../data/invoice.data';
+import { Item } from '../models/item';
 ;
 
 @Injectable({
@@ -13,6 +14,17 @@ export class InvoiceService {
   constructor() { }
 
   getInvoice(): Invoice {
-    return this.invoice;
+    const total = this.calculateTotal();
+    return {... this.invoice, total};
+  }
+
+  calculateTotal() {
+    // una forma de hacerlo:
+    // let total = 0;
+    // this.invoice.items.forEach(item => total += item.price * item.quantity);
+    // return total;
+
+    // otra forma de hacer el calculo del total:
+    return this.invoice.items.reduce((accumulator, item) => accumulator + (item.price * item.quantity), 0)
   }
 }
